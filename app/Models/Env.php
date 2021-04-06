05d2ec39fbc759130da42d2ffd745f72ff787189
Bojitha Piyathilake <bojithapiyathilake@gmail.com>
2021-04-06 12:39:25 +0530

    added gethmis env module

--- app/Models/Env.php
+++ app/Models/Env.php
@@ -10,19 +10,12 @@ class Env extends Model
     use HasFactory;
 
     protected $table = 'eco_systems';
-    protected $fillable = ['ecosystem_type','description','created_by_user_id','status',];
 
-    
-    
-    
-    
-    
-   
+    protected $fillable = ['type_id','title', 'polygon','description', 'created_by_user_id', 'status',];
 
-
-
-
-
-
+    public function ecosystems_type()
+    {
+        return $this->belongsTo('App\Models\Env_type', 'type_id');
+    }
 
 }
--- app/Models/Species.php
+++ app/Models/Species.php
@@ -8,25 +8,22 @@ class Species extends Model
 
 {
     use HasFactory;
-    protected $fillable = ['type', 'title', 'scientefic_name', 'habitats', 'taxa', 'description','status_id',
-];
     protected $table = 'species_information';
 
-
+    protected $fillable = [
+        'type', 'title', 'polygon', 'scientefic_name', 'habitats', 'taxa', 'description', 'status_id',
+    ];
 
     protected $attributes = [
-        'photos' => 0,
-     
-        
-
-
-
-
+        'photos' => "{}",
     ];
     protected $casts = [
         'habitats' => 'array',
         'taxa' => 'array',
-
-
     ];
+
+    public function environment_restoration_species()
+    {
+        return $this->hasMany('App\Models\EnvironmentRestorationSpecies');
+    }
 }
