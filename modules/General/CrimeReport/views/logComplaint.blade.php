cef971cad4c62bf06d406bd9d057f64192a5017b
Bojitha Piyathilake <bojithapiyathilake@gmail.com>
2021-04-22 12:59:48 +0530

    fixing env module layout, adding maps search functionality to all maps

--- modules/General/CrimeReport/views/logComplaint.blade.php
+++ modules/General/CrimeReport/views/logComplaint.blade.php
@@ -229,6 +229,23 @@
         $('#kml').val(tokml(drawnItems.toGeoJSON()));
     });
 
+    //SEARCH FUNCTIONALITY
+    var searchControl = new L.esri.Controls.Geosearch().addTo(map);
+
+    var results = new L.LayerGroup().addTo(map);
+
+    searchControl.on('results', function(data) {
+        results.clearLayers();
+        for (var i = data.results.length - 1; i >= 0; i--) {
+            results.addLayer(L.marker(data.results[i].latlng));
+        }
+    });
+
+    setTimeout(function() {
+        $('.pointer').fadeOut('slow');
+    }, 3400);
+
+    //adding images
     $(document).ready(function() {
         $('#image').change(function() {
             var fp = $("#image");
