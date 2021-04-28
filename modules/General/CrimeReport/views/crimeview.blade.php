2e4b39263a34225e79b9a12b27f0f8c5e2373ffb
Sharuka Perera <sharukaperera98@gmail.com>
2021-04-28 17:15:40 +0530

    fixed crime view issue

--- modules/General/CrimeReport/views/crimeview.blade.php
+++ modules/General/CrimeReport/views/crimeview.blade.php
@@ -33,22 +33,23 @@
     </dl>
     <div id="mapid" style="height:400px;" name="map"></div>
             <div class="row">
-                @isset($Photos)
-                    @if (count($Photos) > 0)
-                            @foreach($Photos as $photo)
-                                <div class="col border border-muted rounded-lg mr-2 p-4">
-                                    <img class="img-responsive" src="{{asset('/storage/'.$photo)}}" alt="photo">
-                                    <a class="nav-link text-dark font-italic p-2" href="/crime-report/downloadimage/{{$photo}}">Download Image</a>
-                                </div>
-                            @endforeach
-                    @endif
-                    @if (count($Photos) < 1)
-                            <p>No photos included in the application</p>
-                    @endif
-                @endisset
-                @empty($Photos)
-                    <p>No photos included in the application</p>
-                @endempty
+            @isset($Photos)
+            <div class="row p-4">
+                <div class="card-deck">
+                    @foreach($Photos as $photo)
+                    <div class="card" style="background-color:#99A3A4">
+                        <img class="card-img-top" src="{{asset('/storage/'.$photo)}}" alt="photo">
+                        <div class="card-body text-center">
+                        <a class="nav-link text-dark font-italic p-2" href="/item-report/downloadimage/{{$photo}}">Download Image</a>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+            </div>
+        @endisset
+        @empty($Photos)
+            <p>No photos included in the application</p>
+        @endempty
             </div>
 
 </div>
