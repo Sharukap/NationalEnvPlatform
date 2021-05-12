6bf94042f6e999bbea51e705ce190625f38a1a0c
Sharuka Perera <sharukaperera98@gmail.com>
2021-05-12 17:53:07 +0530

    Setting up automatic assign

---
+++ app/CustomClass/organization_assign.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\CustomClass;
+ 
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Process_Item;
+use App\Models\District;
+use App\Models\Organization_Activity;
+
+class organization_assign
+{
+    public static function  auto_assign($P_id, $District)
+    {
+      $Process_Item = Process_Item::find($P_id);
+        $activity_org = Organization_Activity::where('district_id',$District)->where('form_type_id',$Process_Item->form_type_id)->first();
+      if($activity_org != null){
+        $Process_Item->activity_organization = $activity_org->organization_id;
+        $Process_Item->status_id=2;
+      }
+      else{
+        $district=District::find($District);
+        $activity_org2 = Organization_Activity::where('province_id',$district->province_id)->where('form_type_id',$Process_Item->form_type_id)->first();
+        if($activity_org2 != null){
+          $Process_Item->activity_organization = $activity_org2->organization_id;
+          $Process_Item->status_id=2;
+        }
+        else{
+            $activity_org3 = Organization_Activity::where('district_id',26)->where('form_type_id',$Process_Item->form_type_id)->first(); //checking for all island
+          if($activity_org3 != null){
+            $Process_Item->activity_organization = $activity_org3->organization_id;
+            $Process_Item->status_id=2;
+          }
+          else{
+            $Process_Item->activity_organization=2;
+          }
+        }
+      }
+      $Process_Item->save();
+      return $Process_Item->activity_organization;
+    }
+}
