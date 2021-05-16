208bc28ef4df6ee315fa715bd653b50043725b74
aselgunaratne <aselgunaratne@gmail.com>
2021-05-16 19:46:17 +0530

    https test 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if (app()->environment('remote')) {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
