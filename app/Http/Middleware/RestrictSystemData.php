41e01bf27aca88f5b88e4f17986c76a114777b2b
aselgunaratne <aselgunaratne@gmail.com>
2021-05-18 19:29:38 +0530

    species bbug test2

--- app/Http/Middleware/RestrictSystemData.php
+++ app/Http/Middleware/RestrictSystemData.php
@@ -26,10 +26,11 @@ class RestrictSystemData
         }
         else{
             $record=Species::find($id);
+            $record1=Species::find(4);
         }
         //dd($process,$id);
         $user = Auth::user()->id;
-        dd($record);
+        dd($record,$id,$record1);
         if($user == $record->created_by_user_id){    
             return back()->with('warning', 'You cannot approve your own records '); 
         }
